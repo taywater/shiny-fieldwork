50cd10ebb1aaaf97ccd188eea80fb90dc2cbab93
Nick <nmanna@akrf.com>
2020-08-12 13:07:10 -0400

    I added global variables to both UI and server in app.R because (and added function(req){}) to UI) because they need to be checked by each every time the app runs, not just when it is deployed.

--- app.R
+++ app.R
@@ -6,6 +6,7 @@
 #it's easier and better to reference columns directly from the db
 #then i'll modify it as "x_table", and that's the table that I feed to dt or reactable to show in the app 
 #these modifications could be changing dates to characters, to show better, rounding decimals, or changing boolean values to be Yes or No
+#I added global variables to both UI and server in app.R because (and added function(req){}) to UI) because they need to be checked by each every time the app runs, not just when it is deployed. 
 
 #load libraries
 library(shiny)
@@ -24,69 +25,10 @@ options(DT.options = list(pageLength = 15))
 #set db connection
 poolConn <- dbPool(odbc(), dsn = "mars_testing", uid = Sys.getenv("shiny_uid"), pwd = Sys.getenv("shiny_pwd"))
 
-# set up (define non-reactive variables to be used throughout modules) ----------------------------------
-
-#query all SMP IDs
-smp_id <- odbc::dbGetQuery(poolConn, paste0("select distinct smp_id from smpid_facilityid_componentid")) %>% 
-  dplyr::arrange(smp_id) %>% 
-  dplyr::pull()
-
-sys_id <- odbc::dbGetQuery(poolConn, paste0("select distinct system_id from smpid_facilityid_componentid")) %>% 
-  dplyr::arrange(system_id) %>% 
-  dplyr::pull()
-
-#query site names (non SMP)
-site_name_query <- "select * from fieldwork.site_name_lookup"
-site_names <- odbc::dbGetQuery(poolConn, site_name_query) %>% 
-                              dplyr::arrange(site_name) %>% 
-                              dplyr::pull()
-
 #disconnect from db on stop 
 onStop(function(){
   poolClose(poolConn)
 })
-
-#Sensor Model Number options
-hobo_options <- c("", "U20-001-01", "U20-001-04", "U20L-01", "U20L-04")
-
-sensor_status_lookup <- dbGetQuery(poolConn, "select * from fieldwork.sensor_status_lookup")
-
-#Sensor Serial Number List
-hobo_list_query <-  "select inv.sensor_serial, inv.sensor_model, inv.date_purchased, ow.smp_id, ow.ow_suffix from fieldwork.inventory_sensors inv
-                          left join fieldwork.deployment d on d.inventory_sensors_uid = inv.inventory_sensors_uid AND d.collection_dtime_est is NULL
-                          left join fieldwork.ow ow on ow.ow_uid = d.ow_uid"
-hobo_list <- odbc::dbGetQuery(poolConn, hobo_list_query)
-sensor_serial <- hobo_list$sensor_serial
-
-#Deployment purpose lookup table
-deployment_lookup <- dbGetQuery(poolConn, "select * from fieldwork.deployment_lookup")
-
-long_term_lookup <- dbGetQuery(poolConn, "select * from fieldwork.long_term_lookup")
-
-research_lookup <- dbGetQuery(poolConn, "select * from fieldwork.research_lookup")
-
-#field test priority
-priority <- dbGetQuery(poolConn, "select * from fieldwork.field_test_priority_lookup")
-
-#srt_types & con phase
-srt_types <- dbGetQuery(poolConn, "select * from fieldwork.srt_type_lookup")
-con_phase <- dbGetQuery(poolConn, "select * from fieldwork.con_phase_lookup")
-
-#porous pavement surface types
-surface_type <- dbGetQuery(poolConn, "select * from fieldwork.surface_type_lookup")
-# 
-# #capture efficiency high flow types
-high_flow_type <- dbGetQuery(poolConn, "select * from fieldwork.est_high_flow_efficiency_lookup")
-
-#this function adds a little red star to indicate that a field is required. It uses HTML, hence "html_req"
-html_req <- function(label){
-  HTML(paste(label, tags$span(style="color:red", "*")))
-}
-
-#monitoring stats
-current_fy <- lubridate::today() %m+% months(6) %>% year()
-start_fy <- 2012
-years <- start_fy:current_fy %>% sort(decreasing = TRUE)
 
 ######  
  #source scripts. each script contains a module, which includes UI and server code
@@ -102,7 +44,66 @@ years <- start_fy:current_fy %>% sort(decreasing = TRUE)
   source("documentation.R")
 
   #call all the UI functions
-  ui <- navbarPage("Fieldwork", theme = shinytheme("cerulean"), id = "inTabset",
+  ui <- function(req){
+    #define global variables that will be required each time the UI runs
+    #query all SMP IDs
+    smp_id <- odbc::dbGetQuery(poolConn, paste0("select distinct smp_id from smpid_facilityid_componentid")) %>% 
+      dplyr::arrange(smp_id) %>% 
+      dplyr::pull()
+    
+    sys_id <- odbc::dbGetQuery(poolConn, paste0("select distinct system_id from smpid_facilityid_componentid")) %>% 
+      dplyr::arrange(system_id) %>% 
+      dplyr::pull()
+    
+    #query site names (non SMP)
+    site_name_query <- "select * from fieldwork.site_name_lookup"
+    site_names <- odbc::dbGetQuery(poolConn, site_name_query) %>% 
+      dplyr::arrange(site_name) %>% 
+      dplyr::pull()
+    
+    #Sensor Model Number options
+    hobo_options <- c("", "U20-001-01", "U20-001-04", "U20L-01", "U20L-04")
+    
+    sensor_status_lookup <- dbGetQuery(poolConn, "select * from fieldwork.sensor_status_lookup")
+    
+    #Sensor Serial Number List
+    hobo_list_query <-  "select inv.sensor_serial, inv.sensor_model, inv.date_purchased, ow.smp_id, ow.ow_suffix from fieldwork.inventory_sensors inv
+                          left join fieldwork.deployment d on d.inventory_sensors_uid = inv.inventory_sensors_uid AND d.collection_dtime_est is NULL
+                          left join fieldwork.ow ow on ow.ow_uid = d.ow_uid"
+    hobo_list <- odbc::dbGetQuery(poolConn, hobo_list_query)
+    sensor_serial <- hobo_list$sensor_serial
+    
+    #Deployment purpose lookup table
+    deployment_lookup <- dbGetQuery(poolConn, "select * from fieldwork.deployment_lookup")
+    
+    long_term_lookup <- dbGetQuery(poolConn, "select * from fieldwork.long_term_lookup")
+    
+    research_lookup <- dbGetQuery(poolConn, "select * from fieldwork.research_lookup")
+    
+    #field test priority
+    priority <- dbGetQuery(poolConn, "select * from fieldwork.field_test_priority_lookup")
+    
+    #srt_types & con phase
+    srt_types <- dbGetQuery(poolConn, "select * from fieldwork.srt_type_lookup")
+    con_phase <- dbGetQuery(poolConn, "select * from fieldwork.con_phase_lookup")
+    
+    #porous pavement surface types
+    surface_type <- dbGetQuery(poolConn, "select * from fieldwork.surface_type_lookup")
+    # 
+    # #capture efficiency high flow types
+    high_flow_type <- dbGetQuery(poolConn, "select * from fieldwork.est_high_flow_efficiency_lookup")
+    
+    #this function adds a little red star to indicate that a field is required. It uses HTML, hence "html_req"
+    html_req <- function(label){
+      HTML(paste(label, tags$span(style="color:red", "*")))
+    }
+    
+    #monitoring stats
+    current_fy <- lubridate::today() %m+% months(6) %>% year()
+    start_fy <- 2012
+    years <- start_fy:current_fy %>% sort(decreasing = TRUE)
+    
+    navbarPage("Fieldwork", theme = shinytheme("cerulean"), id = "inTabset",
                   collection_calendarUI("collection_calendar"),
                   add_owUI("add_ow", smp_id = smp_id, site_names = site_names, html_req = html_req),
                   add_sensorUI("add_sensor", hobo_options = hobo_options, html_req = html_req, sensor_status_lookup = sensor_status_lookup),
@@ -114,9 +115,69 @@ years <- start_fy:current_fy %>% sort(decreasing = TRUE)
                 documentationUI("documentation"),
                  useShinyjs()
   )
+  }
   
   #call modules, referencing the UI names above. These are functions, so any data originating outside the function needs to be named as an argument, whether it is lookup data, or from another tab
   server <- function(input, output, session) {
+    #define global variables that will be defined each time server runs
+    #query all SMP IDs
+    smp_id <- odbc::dbGetQuery(poolConn, paste0("select distinct smp_id from smpid_facilityid_componentid")) %>% 
+      dplyr::arrange(smp_id) %>% 
+      dplyr::pull()
+    
+    sys_id <- odbc::dbGetQuery(poolConn, paste0("select distinct system_id from smpid_facilityid_componentid")) %>% 
+      dplyr::arrange(system_id) %>% 
+      dplyr::pull()
+    
+    #query site names (non SMP)
+    site_name_query <- "select * from fieldwork.site_name_lookup"
+    site_names <- odbc::dbGetQuery(poolConn, site_name_query) %>% 
+      dplyr::arrange(site_name) %>% 
+      dplyr::pull()
+    
+    #Sensor Model Number options
+    hobo_options <- c("", "U20-001-01", "U20-001-04", "U20L-01", "U20L-04")
+    
+    sensor_status_lookup <- dbGetQuery(poolConn, "select * from fieldwork.sensor_status_lookup")
+    
+    #Sensor Serial Number List
+    hobo_list_query <-  "select inv.sensor_serial, inv.sensor_model, inv.date_purchased, ow.smp_id, ow.ow_suffix from fieldwork.inventory_sensors inv
+                          left join fieldwork.deployment d on d.inventory_sensors_uid = inv.inventory_sensors_uid AND d.collection_dtime_est is NULL
+                          left join fieldwork.ow ow on ow.ow_uid = d.ow_uid"
+    hobo_list <- odbc::dbGetQuery(poolConn, hobo_list_query)
+    sensor_serial <- hobo_list$sensor_serial
+    
+    #Deployment purpose lookup table
+    deployment_lookup <- dbGetQuery(poolConn, "select * from fieldwork.deployment_lookup")
+    
+    long_term_lookup <- dbGetQuery(poolConn, "select * from fieldwork.long_term_lookup")
+    
+    research_lookup <- dbGetQuery(poolConn, "select * from fieldwork.research_lookup")
+    
+    #field test priority
+    priority <- dbGetQuery(poolConn, "select * from fieldwork.field_test_priority_lookup")
+    
+    #srt_types & con phase
+    srt_types <- dbGetQuery(poolConn, "select * from fieldwork.srt_type_lookup")
+    con_phase <- dbGetQuery(poolConn, "select * from fieldwork.con_phase_lookup")
+    
+    #porous pavement surface types
+    surface_type <- dbGetQuery(poolConn, "select * from fieldwork.surface_type_lookup")
+    # 
+    # #capture efficiency high flow types
+    high_flow_type <- dbGetQuery(poolConn, "select * from fieldwork.est_high_flow_efficiency_lookup")
+    
+    #this function adds a little red star to indicate that a field is required. It uses HTML, hence "html_req"
+    html_req <- function(label){
+      HTML(paste(label, tags$span(style="color:red", "*")))
+    }
+    
+    #monitoring stats
+    current_fy <- lubridate::today() %m+% months(6) %>% year()
+    start_fy <- 2012
+    years <- start_fy:current_fy %>% sort(decreasing = TRUE)
+    
+    
     collection_cal <- callModule(collection_calendar, "collection_calendar", parent_session = session, ow = ow, deploy = deploy, poolConn = poolConn)
     ow <- callModule(add_ow, "add_ow", parent_session = session, smp_id = smp_id, poolConn = poolConn)
     sensor <- callModule(add_sensor, "add_sensor", parent_session = session, poolConn = poolConn, sensor_status_lookup = sensor_status_lookup)
